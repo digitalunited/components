aad3852b94a3072dfd2740fa57700498c8b30339
Mikael Engström <mikael.engstrom@careofhaus.se>
2015-02-03 13:09:37 +0100

    initial commit

---
+++ Components.php
@@ -0,0 +1,11 @@
+<?php
+/**
+Plugin Name: Components
+Plugin URI: https://github.com/digitalunited/components
+Author: Digital United
+Author URI: http://digitalunited.io
+*/
+
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Components #
+@todo write readme
