0bf5f938cfc05a942ff02d16743184e663d23e91
mien <mien@Mikael-Engstroms-MacBook-Pro.local>
2015-02-15 10:39:30 +0100

    adds text-domain to components

--- Components.php
+++ Components.php
@@ -10,6 +10,8 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require __DIR__ . '/vendor/autoload.php';
 }
 
+load_theme_textdomain('components', get_template_directory() . '/lang');
+
 $autoloader = new \DigitalUnited\Components\Autoloader();
 $autoloader->requireFiles();
 $autoloader->registerComponents();
