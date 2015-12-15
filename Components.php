7f7e45ba43a745c140668a2d71753324705c954f
Jocke <joakim.hising@careofhaus.se>
2015-12-15 11:18:05 +0100

    Changed to template path

--- Components.php
+++ Components.php
@@ -10,7 +10,7 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require __DIR__ . '/vendor/autoload.php';
 }
 
-load_theme_textdomain('components', get_stylesheet_directory() . '/lang');
+load_theme_textdomain('components', get_template_directory() . '/lang');
 
 $autoloader = new \DigitalUnited\Components\Autoloader();
 $autoloader->requireFiles();
