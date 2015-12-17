06b134b78499a71c55a2d4a2023decc129a59229
Lucas Andersson <lucas@careofhaus.se>
2015-12-17 13:34:09 +0100

    fixes for translations in child theme

--- Components.php
+++ Components.php
@@ -11,6 +11,7 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 }
 
 load_theme_textdomain('components', get_template_directory() . '/lang');
+load_child_theme_textdomain('components', get_stylesheet_directory() . '/lang');
 
 $autoloader = new \DigitalUnited\Components\Autoloader();
 $autoloader->requireFiles();
